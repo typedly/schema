5fe2711141d5f5fdc28ebd0efb8e3dabe1910f84
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 23:44:51 +0000

    refactor: improve comments and structure in SchemaValue type definition

--- src/lib/schema-value.type.ts
+++ src/lib/schema-value.type.ts
@@ -8,14 +8,20 @@ import type { SchemaTypeMap } from "./schema-type-map.type";
  * @template V 
  */
 export type SchemaValue<V> =
+  // Handle array types first. e.g. { array: 'string' } => string[]
   V extends { array: infer Arr }
     ? Arr extends keyof SchemaTypeMap
+      // Handle native array types. e.g. { array: 'string' } => string[]
       ? SchemaTypeMap[Arr][]
+      // Handle nested array types. e.g. { array: { array: 'string' } } => string[][]
       : Arr extends Record<string, any>
         ? ExpandDeep<SchemaToType<Arr>>[]
         : never[]
+  // Handle primitive types. e.g. 'string' => string
   : V extends keyof SchemaTypeMap
     ? SchemaTypeMap[V]
-  : V extends Record<string, any>
-    ? ExpandDeep<SchemaToType<V>>
-  : never;
+    : V extends readonly any[]  // native TS array type
+      ? V
+      : V extends Record<string, any>
+        ? ExpandDeep<SchemaToType<V>>
+      : never;
