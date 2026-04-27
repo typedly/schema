e24466775754d8291fdf9db791b1b33198a9ce00
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 23:45:34 +0000

    refactor: enhance comments and structure in ExpandDeep type definition

--- src/lib/expand-deep.type.ts
+++ src/lib/expand-deep.type.ts
@@ -4,8 +4,13 @@
  * @template T 
  */
 export type ExpandDeep<T> =
+  // Handle function types. e.g. () => void => () => void
   T extends (...args: unknown[]) => unknown ? T
+  // Handle Date type. e.g. Date => Date
   : T extends Date ? T
+  // Handle array types. e.g. string[] => string[]
   : T extends readonly (infer U)[] ? ExpandDeep<U>[]
-  : T extends object ? { [K in keyof T]: ExpandDeep<T[K]> }
+  // Handle object types. e.g. { name: string } => { name: string }
+  : T extends object ? { -readonly [K in keyof T]: ExpandDeep<T[K]> }
+  // Handle primitive types. e.g. string => string
   : T;
