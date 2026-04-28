9a318bcce51d262da6f6f2e08fa48bdfd9e16329
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-28 01:07:03 +0000

    chore(api): update public API to remove unused types and include SchemaRecordToType

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,4 @@
-export type { ExpandDeep } from './expand-deep.type';
-export type { IsOptional } from './is-optional.type';
-export type { RemoveOptional } from './remove-optional.type';
+export type { SchemaRecordToType } from './schema-record-to-type.type';
 export type { SchemaToType } from './schema-to-type';
 export type { SchemaTypeMap } from './schema-type-map.type';
 export type { SchemaValue } from './schema-value.type';
